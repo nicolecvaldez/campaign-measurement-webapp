20a762e63259933c1376b3b9c7a74b621b1d8607
nicolecvaldez <nicolecvaldez@gmail.com>
2018-08-03 14:47:57 +0800

    Added secret key because the error kept raising.

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,10 @@ UPLOAD_FOLDER = '/Users/nicole/git/campaign-measurement-webapp/app/uploads'
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config.update(dict(
+    SECRET_KEY="karai_tokusei",
+    WTF_CSRF_SECRET_KEY="five_piece_gyoza"
+))
 Bootstrap(app)
 
 from app import views
